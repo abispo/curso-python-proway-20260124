181581afd5784d203e46a4e6a985f257b23d77f0
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2026-02-07 11:38:44 -0300

    Pacote estatisticas

--- modulo03-aplicacoes-web-com-python/aula03/meusite/urls.py
+++ modulo03-aplicacoes-web-com-python/aula03/meusite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('enquetes/', include('enquetes.urls'))
+    path('enquetes/', include('enquetes.urls')),
+    path('estatisticas/', include('estatisticas.urls'))
 ]
