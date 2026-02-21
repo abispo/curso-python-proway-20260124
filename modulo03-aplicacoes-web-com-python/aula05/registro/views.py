eb4c065271e71ffa2d45917443334f03e0f39010
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2026-02-21 09:15:49 -0300

    Criação da model PreRegistro

--- modulo03-aplicacoes-web-com-python/aula05/registro/views.py
+++ modulo03-aplicacoes-web-com-python/aula05/registro/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
+from django.urls import reverse
 
 from . import forms
 
+# Apesar de não ser obrigatório, podemos indicar o tipo dos parâmetros de funções e também o tipo de retorno. Muitos consideram uma boa prática, e é muito útil também no uso de IDEs que não conseguem automaticamente inferir o tipo de dado que está sendo tratado.
 def pre_registro(request: HttpRequest) -> HttpResponse:
-    return render(
-        request,
-        "registro/pre_registro.html",
-        {"form": forms.PreRegistroForm}
-    )
+    if request.method == "GET":
+        return render(
+            request,
+            "registro/pre_registro.html",
+            {"form": forms.PreRegistroForm}
+        )
+    
+    elif request.method == "POST":
+        form = forms.PreRegistroForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+
+            return redirect(reverse(
+                "registro:pre_registro"
+            ))
