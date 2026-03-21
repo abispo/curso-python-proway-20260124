3103c9106411a67aea30901a45602c6a8d42e5cc
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2026-03-21 08:54:32 -0300

    Página de criação de ordem de serviço

--- modulo03-aplicacoes-web-com-python/aula09/gestao/urls.py
+++ modulo03-aplicacoes-web-com-python/aula09/gestao/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "gestao"
 
 urlpatterns = [
-    path('', views.index, name="index"),   
+    path('', views.index, name="index"),
+    path('nova-os/', views.nova_ordem_de_servico, name="nova_ordem_de_servico")
 ]
--- modulo03-aplicacoes-web-com-python/aula09/gestao/views.py
+++ modulo03-aplicacoes-web-com-python/aula09/gestao/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.http.request import HttpRequest
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-def index(request):
+from .models import OrdemDeServico
+
+def index(request: HttpRequest):
     return render(
         request,
         "gestao/index.html"
     )
+
+def nova_ordem_de_servico(request: HttpRequest):
+
+    if request.method == "GET":
+        return render(
+            request,
+            "gestao/nova_ordem_de_servico.html"
+        )
+    
+    elif request.method == "POST":
+
+        titulo = request.POST.get("titulo")
+        descricao = request.POST.get("descricao")
+
+        os = OrdemDeServico(
+            titulo=titulo,
+            descricao=descricao,
+            cliente=request.user
+        )
+
+        os.save()
+
+        return redirect(reverse("gestao:nova_ordem_de_servico"))
